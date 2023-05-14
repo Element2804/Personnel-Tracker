21142b5285ad427bcdccbff5652ba6a573e55713
Element2804 <103654389+Element2804@users.noreply.github.com>
2023-05-14 16:10:50 -0400

    fixed schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,23 +7,25 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Computer Technician', 70000, 1),
 ('Lead Technician', 80000, 1),
-('Researcher', 70000, 2), 
+('Computer Technician', 70000, 1),
 ('Research & development Manager', 85000, 2),
+('Researcher', 70000, 2),
+('Sales Coordinator', 90000, 3), 
 ('Salesperson', 60000, 3), 
-('Sales Coordinator', 90000, 3),
-('Process Engineer', 80000, 4),
-('Project Manager', 1000000, 4);
+('Project Manager', 1000000, 4),
+('Process Engineer', 80000, 4);
+
 
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-('Thakul', 'Llaidmon', 1, 2),
-('Chuck', 'Noris', 2, null),
-('Downtown', 'Murphybrown', 3, 4),
-('Crangis', 'McBasketball', 4, null),
-('Andy', 'Howson', 5, 6),
-('Liam', 'Harrison', 6, null),
-('Duke', 'Nukem', 7, 8),
-('Rafael', 'Ninturtle', 8, null);
+('Chuck', 'Noris', 1, null),
+('Thakul', 'Llaidmon', 2, 1),
+('Crangis', 'McBasketball', 3, null),
+('Downtown', 'Murphybrown', 4, 3),
+('Liam', 'Harrison', 5, null),
+('Andy', 'Howson', 6, 5),
+('Rafael', 'Ninturtle', 7, null),
+('Duke', 'Nukem', 8, 7);
+
